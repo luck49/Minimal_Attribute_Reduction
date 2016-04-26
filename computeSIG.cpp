9fc68c75660d6a9cbf836a038b4eace2bff700de
Brick.Lee <BrickLees@163.com>
2016-04-26 22:01:09 +0800

    Added files via upload
    compute the significance of attribute

---
+++ computeSIG.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include"matrix.h"
+using namespace std;
+void computeSIG(class matrix& Sig,const class matrix& DF){
+	 Sig=*(new matrix(3,DF.row));
+	 Sig.mat.row(1)=DF.mat.row(1);//存储DT属性序
+	 for(int i=0;i<Sig.row;i++){//计算出现频率 
+	 	Sig.mat(2,i)=i;
+	 	Sig.mat(3,i)=0;
+	 	for(int j=0;j<line;j++)
+	 		Sig.mat(3,i)+=DF.mat(j,i);
+	 }
+	 //依据属性出现的频率对属性选择排序
+	 int index;
+	 MatrixXd temp(3,1);
+	 for(int i=0;i<row;i++){
+	 	index=i;
+	 	for(int j=i+1;j<row;j++){
+	 		if(Sig.mat(3,index)<Sig.mat(3,j))
+	 			index=j;
+		 }
+		 if(index!=i){
+		 	temp=Sig.mat.col(index);
+		 	Sig.mat.col(index)=Sig.mat.col(i);
+		 	Sig.mat.col(i)=temp;
+		 } 
+	 } 
+} 
